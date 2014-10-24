fc6161bbeb6b1e0e84a87311390b286095e4a358
Pavithra <pavithra@mouse.(none)>
2014-10-24 15:17:51 +0530

    Simulataneous sending implemented TODO: bernoauli and speed up factor

--- working_PIM.py
+++ working_PIM.py
@@ -1,3 +1,4 @@
+import thread
 import sys
 import time
 from random import randint
@@ -16,7 +17,7 @@ def setPos(turt,back,up):
 	
 def drawcicle(noOfInterface,val):
 	tlist = list()
-	screen = turtle.getscreen()
+	screen = turtle.getscreen()	
 	screen.tracer(0)
 	for i in range(noOfInterface):
 	    	tlist.append(turtle.Turtle())
@@ -26,7 +27,7 @@ def drawcicle(noOfInterface,val):
 		tlist[i].end_fill()
 		tlist[i].hideturtle() 
 	for i in range(noOfInterface):
-	    	turt = turtle.Turtle()
+		turt = turtle.Turtle()
 		setPos(turt,val-300,200-(10+90*i))
 		turt.begin_fill()
 		turt.circle(15)
@@ -35,30 +36,62 @@ def drawcicle(noOfInterface,val):
 	screen.update()
 	
 
+def drawVOQ(noOfInterface,val):
+	tlist = list()
+	screen = turtle.getscreen()	
+	screen.tracer(0)
+	count = 0 
+	for i in range(noOfInterface):
+		for j in range(noOfInterface):
+	  	  	tlist.append(turtle.Turtle())
+			setPos(tlist[count],val+5,210-(10+90*i+j*10))
+			if j in voqList[i]:
+				tlist[count].begin_fill()
+			for k in range(2):
+				tlist[count].forward(7)
+				tlist[count].left(90)
+				tlist[count].forward(30)
+				tlist[count].left(90)
+			if j in voqList[i]:
+				tlist[count].end_fill()
+			tlist[count].hideturtle() 
+			count += 1
+	screen.update()
+
 def initialiseDraw(array,noOfInterface,isGrant,val):
 	x = list()
 	y = list()
 	count = 0
+	acceptCount = 0
 	for i in range(noOfInterface):
 		for j in array[i]:
 			x.append(10+i*90)
 			y.append(10+j*90)
 			count+=1
 	if isGrant == 0:
-		draw(x,y,count,0,val)
+		acceptCount = count+1
+		for i in range(noOfInterface):
+			for j in acceptance[i]:
+				x.append(10+i*90)
+				y.append(10+j*90)
+				count += 1
+				acceptance[i].remove(j)
+
+	if isGrant == 1:
+		draw(y,x,count,1,val,acceptCount)
 	else:
-		draw(y,x,count,1,val)
+		draw(x,y,count,isGrant,val,acceptCount)
 
 def drawText(text,back,up):
 	screen = turtle.getscreen()
-	screen.tracer(0)
 	turt = turtle.Turtle()
+	screen.tracer(0)
 	setPos(turt,back,up)
 	turt.write(text,font=("Arial", 20, "normal"))
 	turt.hideturtle()
 	screen.update()
 	
-def draw(x,y,count,isGrant,val):
+def draw(x,y,count,isGrant,val,acceptCount):
 	tlist = list()
 	p = list()
 	screen = turtle.getscreen()
@@ -66,7 +99,11 @@ def draw(x,y,count,isGrant,val):
 	for i in range(count):
 	    screen.tracer(10)
 	    tlist.append(turtle.Turtle())
+	    #print "here",i,acceptCount,count
 	    tlist[i].color("red")
+	    if i >= acceptCount-1 and acceptCount!= 0:
+		tlist[i].shape("square")
+	    	tlist[i].color("green")
 	    tlist[i].speed(1)
 	    tlist[i].width(4)
 	    angle = math.atan((y[i]-x[i])/300.0)
@@ -92,11 +129,16 @@ def draw(x,y,count,isGrant,val):
 
 noOfInterface = int(raw_input("Enter the number of port "))
 inputport = [[] for i in range(noOfInterface)]
+voqList = [[] for i in range(noOfInterface)]
+acceptance = [[] for i in range(noOfInterface)]
 flag = 1
 for i in range(noOfInterface):
 	ports = randint(0,noOfInterface)
 	for l in range(ports):
-		inputport[i].append(randint(0,noOfInterface-1))
+		no = randint(0,noOfInterface-1)
+		if no not in inputport[i]:
+			inputport[i].append(no)
+			voqList[i].append(no)
 	print "input = ",inputport[i]
 
 def callRequest (inputport,noOfInterface):
@@ -114,24 +156,25 @@ def callGrant(request,noOfInterface):
 	#		print "grant ",request[i][rand],i
 	return grant
 
+
 def callAcceptance(grant,noOfInterface):
 	count = 0
 	x = list()
 	y = list()
+	drawText("ACCEPT",-350,225)
+	drawcicle(noOfInterface,-300)
+	drawVOQ(noOfInterface,-250)
 	for i in range(noOfInterface):
 		if(len(grant[i]) > 0):
 			rand = randint(0,len(grant[i])-1)
 			print "Input port ",i+1,"accepted grant from ",grant[i][rand]+1
-			x.append(10+i*90)
-			y.append(10+grant[i][rand]*90)
-			count+= 1
+			acceptance[i].append(grant[i][rand])
+			voqList[i].remove(grant[i][rand])
 			del inputport[i][:]
 			for j in range(noOfInterface):
 				if grant[i][rand] in inputport[j]:
 					inputport[j].remove(grant[i][rand])
-	drawText("ACCEPT",-350,225)
-	drawcicle(noOfInterface,-300)
-	draw(x,y,count,0,-300)
+	initialiseDraw(acceptance,noOfInterface,2,-300)	
 	return inputport			
 itr = 1
 while flag == 1:
@@ -139,18 +182,31 @@ while flag == 1:
 	drawText(txt,150,300)
 	grant = [[] for i in range(noOfInterface)]
 	request = [[] for i in range(noOfInterface)]
-	drawText("REQUEST",550,225)
+	if itr == 1:
+		drawText("REQUEST",550,225)
+	else:
+		drawText("REQUEST AND SENDING",550,225)
+	
 	drawcicle(noOfInterface,550)
+	drawVOQ(noOfInterface,600)
 	request = callRequest(inputport,noOfInterface)
 	initialiseDraw(inputport,noOfInterface,0,550)	
 	grant = callGrant(request,noOfInterface)
 	drawText("GRANT",100,225)
 	drawcicle(noOfInterface,125)
+	drawVOQ(noOfInterface,175)
 	initialiseDraw(grant,noOfInterface,1,-150)
 	inputport = callAcceptance(grant,noOfInterface)
+#	if itr != 1:
+#		thread.start_new_thread(drawSending, (noOfInterface,acceptance) )
 	flag = 0 
 	for i in range(noOfInterface):
 		if (len(inputport[i]) != 0):
 			flag = 1
 	itr+=1
-	turtle.done()
+	turtle.exitonclick()
+drawText("SENDING",550,225)
+drawcicle(noOfInterface,550)
+drawVOQ(noOfInterface,600)
+initialiseDraw(inputport,noOfInterface,0,550)	
+turtle.exitonclick()
